eef4fcb051ec4c2ffc2888130e55bdb55796e807
Daisy Sanchez <sanchez.daisy86@gmail.com>
2021-07-19 03:39:23 -0700

    modified exit in cat.c user file

--- cat.c
+++ cat.c
@@ -12,12 +12,12 @@ cat(int fd)
   while((n = read(fd, buf, sizeof(buf))) > 0) {
     if (write(1, buf, n) != n) {
       printf(1, "cat: write error\n");
-      exit();
+      exit(1); //LAB1A error has occurred here//
     }
   }
   if(n < 0){
     printf(1, "cat: read error\n");
-    exit();
+    exit(1); //LAB1A error, exit 1//
   }
 }
 
@@ -28,16 +28,16 @@ main(int argc, char *argv[])
 
   if(argc <= 1){
     cat(0);
-    exit();
+    exit(1); //LAB1A error bc argc must be at least 2 in order to cat//
   }
 
   for(i = 1; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       printf(1, "cat: cannot open %s\n", argv[i]);
-      exit();
+      exit(1); //LAB1A error ^//
     }
     cat(fd);
     close(fd);
   }
-  exit();
+  exit(0); //LAB1A reached end, success//
 }
