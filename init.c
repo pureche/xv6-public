d7f80aa950bff3bc728c1781badc99ca6b1720a0
Daisy Sanchez <sanchez.daisy86@gmail.com>
2021-07-19 00:47:57 -0700

    edit exit calls in init

--- init.c
+++ init.c
@@ -24,12 +24,12 @@ main(void)
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
-      exit();
+      exit(1); //exit 1 bc failed//
     }
     if(pid == 0){
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
-      exit();
+      exit(1); //exit 1 status bc failed//
     }
     while((wpid=wait()) >= 0 && wpid != pid)
       printf(1, "zombie!\n");
